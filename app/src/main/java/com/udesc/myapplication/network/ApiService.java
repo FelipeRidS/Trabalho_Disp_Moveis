cd21d1de842c2546cdfd111277daad5a2a8196db
Igor José <igor.mota@kugel.com.br>
2025-11-20 15:22:00 -0300

    requisição de cadastro de usuário

--- app/src/main/java/com/udesc/myapplication/network/ApiService.java
+++ app/src/main/java/com/udesc/myapplication/network/ApiService.java
@@ -2,6 +2,7 @@ package com.udesc.myapplication.network;
 
 import com.udesc.myapplication.model.LoginRequest;
 import com.udesc.myapplication.model.Usuario;
+import com.udesc.myapplication.model.UsuarioCreateRequest;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -12,7 +13,6 @@ public interface ApiService {
     @POST("usuario/login")
     Call<Usuario> login(@Body LoginRequest loginRequest);
 
-    // Adicione outros endpoints aqui (ex: criar conta)
-    // @POST("usuario")
-    // Call<Usuario> criarConta(@Body Usuario novoUsuario);
+    @POST("usuario")
+    Call<Usuario> registrar(@Body UsuarioCreateRequest usuarioCreateRequest);
 }
